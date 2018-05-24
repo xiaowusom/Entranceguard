4aed1b145d208c6f4a83175e9d9a26ca2e1d01ee
伍健 <wujian@wujiandeMacBook-Air.local>
2018-05-24 16:46:27 +0800

    提交

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var md5 = require('md5');
+var moment = require('moment');
+var router = express.Router();
+
+/* GET home page. */
+//router.get('/openDoor/:phone/:projectCode/:token/:userName', function(req, res, next) {
+//
+//  res.render('typeError', {
+//  });
+//});
+
+router.get('/openDoor/:phone/:projectCode/:token/:type/:userName', function(req, res) {
+		var currentdate = moment().utc().utcOffset(+8).format("YYYY-MM-DD-HH");
+		var hash = md5(currentdate+"adminXH")
+		console.log("日期"+currentdate)
+		console.log("node_token"+"----"+hash)
+		console.log("app_token"+"----"+req.params.token)
+		 
+		if(hash===req.params.token){
+			var phone = req.params.phone
+		    var projectCode = req.params.projectCode
+		    var userName = req.params.userName
+		    var type = req.params.type
+		    res.render('index', {
+		        phone: phone,
+		        projectCode: projectCode,
+		        userName:userName,
+		        type:type
+		    })
+		}else{
+			 res.render('error', {
+		    	
+		    })
+		}   
+});
+
+module.exports = router;
+
+
